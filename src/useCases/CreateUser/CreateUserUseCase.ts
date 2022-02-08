32cdb7144b03fd074c476506c12691d968c3437f
Moisés Mariano <moisesduartem@gmail.com>
2022-02-07 21:21:45 -0300

    feat: create imailprovider

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,10 +1,12 @@
 import { User } from '../../entities/User';
+import { IMailProvider } from '../../providers/IMailProvider';
 import { IUsersRepository } from '../../repositories/IUsersRepository';
 import { ICreateUserRequestDTO } from './ICreateUserRequestDTO';
 
 export class CreateUserUseCase {
   public constructor(
     private usersRepository: IUsersRepository,
+    private mailProvider: IMailProvider,
   ) {}
 
   public async execute(data: ICreateUserRequestDTO) {
@@ -17,5 +19,18 @@ export class CreateUserUseCase {
     const user = new User(data);
 
     await this.usersRepository.save(user);
+
+    this.mailProvider.sendMail({
+      to: {
+        name: data.name,
+        email: data.email,
+      },
+      from: {
+        name: 'Equipe do meu app',
+        email: 'equipe@meuapp.com',
+      },
+      subject: 'Seja bem-vindo à plataforma',
+      body: '<p>Você já pode fazer login em nossa plataforma.</p>',
+    });
   }
 }
